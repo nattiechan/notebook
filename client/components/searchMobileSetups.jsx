f86e29bcccf7c81fbabf6f9e32b8f2d522cb9733
Nattie C <47332368+nattiechan@users.noreply.github.com>
2023-03-02 16:51:34 -0800

    searchMobileSetup: use destructing assignment

--- client/components/searchMobileSetups.jsx
+++ client/components/searchMobileSetups.jsx
@@ -2,53 +2,41 @@ import React from 'react';
 
 function SearchMobileSetups(props) {
     const { record } = props;
-    const hasSameStrings = record.main_string === record.cross_string;
-    const strings = hasSameStrings
-        ? record.main_string : `${record.main_string} / ${record.cross_string}`;
-    const tension = record.main_tension === record.cross_tension
-        ? record.main_tension : `${record.main_tension} / ${record.cross_tension}`;
+    const {
+        main_string,
+        cross_string,
+        main_tension,
+        cross_tension,
+        main_pre_stretch,
+        cross_pre_stretch,
+        num_of_racquets,
+        tension_measuring_unit
+    } = record;
+    const hasSameStrings = main_string === cross_string;
+    const strings = hasSameStrings ? main_string : `${main_string} / ${cross_string}`;
+    const tension = main_tension === cross_tension
+        ? main_tension : `${main_tension} / ${cross_tension}`;
 
     const getPreStretch = () => {
-        if (record.main_pre_stretch === '' && record.cross_pre_stretch === '') {
+        if (main_pre_stretch === '' && cross_pre_stretch === '') {
             return '';
-        } else if (record.main_pre_stretch !== '' && record.cross_pre_stretch !== '') {
-            return `${record.main_pre_stretch} / ${record.cross_pre_stretch} PS`;
-        } else if (record.cross_pre_stretch === '') {
-            return `${record.main_pre_stretch} PS main`;
+        } else if (main_pre_stretch !== '' && cross_pre_stretch !== '') {
+            return `${record.main_pre_stretch} / ${cross_pre_stretch} PS`;
+        } else if (cross_pre_stretch === '') {
+            return `${main_pre_stretch} PS main`;
         } else {
-            return `${record.cross_pre_stretch} PS cross`;
+            return `${cross_pre_stretch} PS cross`;
         }
     }
 
-    const sameStringElement = () => {
-        return (
-            <>
-                <span id='num-of-racquets'>{record.num_of_racquets} x{' '}</span>
-                <span id='setup'>
-                    {strings}{' '}
-                    @ {tension} {record.tension_measuring_unit}{' '}
-                    {getPreStretch()}
-                </span>
-            </>
-        );
-    }
-
-    const differentStringElement = () => {
-        return (
-            <>
-                <span id='num-of-racquets'>{record.num_of_racquets} x{' '}</span>
-                <span id='setup'>
-                    {strings}<br />
-                    @ {tension} {record.tension_measuring_unit}{' '}
-                    {getPreStretch()}
-                </span>
-            </>
-        );
-    };
-
     return (
         <div id='combo'>
-            {hasSameStrings ? sameStringElement() : differentStringElement()}
+            <span id='num-of-racquets'>{num_of_racquets} x{' '}</span>
+            <span id='setup'>
+                {strings}{hasSameStrings ? ' ' : <br />}
+                @ {tension} {tension_measuring_unit}{' '}
+                {getPreStretch()}
+            </span>
         </div>
     );
 }
